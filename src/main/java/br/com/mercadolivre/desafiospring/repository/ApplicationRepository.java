8b17960a5cadb22f19f10cc3f30e0750559e5fe7
esutil <evandro.sutil@mercadolivre.com>
2022-03-23 18:55:50 -0300

    feat: add product service
    -
    
    start: #3

--- src/main/java/br/com/mercadolivre/desafiospring/repository/ApplicationRepository.java
+++ src/main/java/br/com/mercadolivre/desafiospring/repository/ApplicationRepository.java
@@ -3,7 +3,6 @@ package br.com.mercadolivre.desafiospring.repository;
 import java.util.List;
 import java.util.Optional;
 
-
 public interface ApplicationRepository<T, K> {
     public List<T> read();
     public Optional<T> find(K id);
