45c9496c6d64e4deb417eb406e0da6fe143f9dfe
Icaro-Salgado <icaro.salgado@mercadolivre.com>
2022-03-23 16:12:08 -0300

    iniciando projeto

---
+++ src/main/java/br/com/mercadolivre/desafiospring/DesafioSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.mercadolivre.desafiospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/mercadolivre/desafiospring/DesafioSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mercadolivre.desafiospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
