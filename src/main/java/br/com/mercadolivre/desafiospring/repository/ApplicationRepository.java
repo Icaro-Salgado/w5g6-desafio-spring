a90ae83a16f3953e3d46d94c6dc6d27678356be9
pedroLSoares <pedro.levada@mercadolivre.com>
2022-03-23 17:09:11 -0300

    Add generic for ID field

--- src/main/java/br/com/mercadolivre/desafiospring/repository/ApplicationRepository.java
+++ src/main/java/br/com/mercadolivre/desafiospring/repository/ApplicationRepository.java
@@ -4,9 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 
-public interface ApplicationRepository<T> {
+public interface ApplicationRepository<T, K> {
     public List<T> read();
-    public Optional<T> find(int id);
+    public Optional<T> find(K id);
     public void add(T toAdd);
     public void add(List<T> listToAdd);
 }
