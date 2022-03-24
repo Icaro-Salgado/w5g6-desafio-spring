22f33a40db733b677364f563ac8e2bd90fc9835a
Icaro-Salgado <icaro.salgado@mercadolivre.com>
2022-03-24 09:32:05 -0300

    feat: add client controller
    -
    
    starts #19

---
+++ src/main/java/br/com/mercadolivre/desafiospring/controller/ClientController.java
@@ -0,0 +1,11 @@
+package br.com.mercadolivre.desafiospring.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/")
+@RequiredArgsConstructor
+public class ClientController {
+}
