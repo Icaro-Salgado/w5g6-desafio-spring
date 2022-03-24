91e536c4e99075b3f9ecd5ce9fe8f6f10d3837e8
ThainanEsteves <thainan.santos@mercadolivre.com>
2022-03-24 11:33:31 -0300

    feat: create purchase model
    -
    
    closed : #15

---
+++ src/main/java/br/com/mercadolivre/desafiospring/services/PurchaseService.java
@@ -0,0 +1,23 @@
+package br.com.mercadolivre.desafiospring.services;
+
+
+import br.com.mercadolivre.desafiospring.models.Purchase;
+import br.com.mercadolivre.desafiospring.repository.ApplicationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PurchaseService {
+
+//    private final ApplicationRepository<Purchase, Long> repo;
+//
+//    public List<Purchase> addPurchase(List<Purchase> purchase) throws IOException {
+//        //epo.add(purchase);
+//        return purchase;
+//    }
+
+}
