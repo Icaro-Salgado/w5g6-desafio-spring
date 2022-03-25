0a57e69a7702ed77ec9b8687690f490c7bc8952f
Icaro-Salgado <icaro.salgado@mercadolivre.com>
2022-03-25 15:32:53 -0300

    feat: creating a RestControllerAdvice to handle DB exceptions
    start #39

---
+++ src/main/java/br/com/mercadolivre/desafiospring/controller/advice/DBExceptionHandler.java
@@ -0,0 +1,24 @@
+package br.com.mercadolivre.desafiospring.controller.advice;
+
+import br.com.mercadolivre.desafiospring.exceptions.db.DataBaseReadException;
+import br.com.mercadolivre.desafiospring.exceptions.db.DataBaseWriteException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class DBExceptionHandler {
+    // TODO: Criar uma exceção para escrita e uma para leitura
+
+    @ExceptionHandler(value = {DataBaseWriteException.class})
+    protected ResponseEntity<Object> handleWriteException(DataBaseWriteException ex) {
+        String bodyOfResponse = ex.getMessage();
+        return ResponseEntity.badRequest().body(bodyOfResponse);
+    }
+
+    @ExceptionHandler(value = {DataBaseReadException.class})
+    protected ResponseEntity<Object> handleReadException(DataBaseReadException ex) {
+        String bodyOfResponse = ex.getMessage();
+        return ResponseEntity.badRequest().body(bodyOfResponse);
+    }
+}
