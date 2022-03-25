e61639adfe04cffb62031602ca56701ac7e37bae
Klinton Lee <klinton.lee@mercadolivre.com>
2022-03-25 19:44:11 -0300

    Update src/main/java/br/com/mercadolivre/desafiospring/exceptions/InvalidBodyContentException.java

--- src/main/java/br/com/mercadolivre/desafiospring/exceptions/InvalidBodyContentException.java
+++ src/main/java/br/com/mercadolivre/desafiospring/exceptions/InvalidBodyContentException.java
@@ -5,7 +5,6 @@ import lombok.Data;
 @Data
 public class InvalidBodyContentException extends RuntimeException{
 
-    private static final long serialVersionUID = -1957137827451037216L;
     private Object invalidBody;
 
     public InvalidBodyContentException(Object body) {
