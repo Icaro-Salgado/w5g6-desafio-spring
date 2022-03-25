0b2483e02fd0ddb883ef7ff5a6332274bcc172c8
Klinton Sae Hyung Lee <klinton.lee@mercadolivre.com>
2022-03-25 14:06:58 -0300

    feat: add a new route that sum the price of all customer products
    -
    
    start: #24

--- src/main/java/br/com/mercadolivre/desafiospring/controller/PurchaseController.java
+++ src/main/java/br/com/mercadolivre/desafiospring/controller/PurchaseController.java
@@ -1,15 +1,13 @@
 package br.com.mercadolivre.desafiospring.controller;
 
 
+import br.com.mercadolivre.desafiospring.dto.request.CartDTO;
 import br.com.mercadolivre.desafiospring.dto.request.PurchaseDTO;
 import br.com.mercadolivre.desafiospring.dto.request.PurchaseRequestDTO;
 import br.com.mercadolivre.desafiospring.models.Purchase;
 import br.com.mercadolivre.desafiospring.services.PurchaseService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -28,6 +26,18 @@ public class PurchaseController {
 
         return requestFromPurchases.stream().map(PurchaseDTO::new).collect(Collectors.toList());
 
+    }
+
+    @PostMapping("/cart/{customerId}")
+    public CartDTO effectiveCart(
+            @PathVariable Long customerId
+    ) throws IOException, NoSuchMethodException {
+        List<Purchase> purchases = purchaseService.findCustomerPurchases(customerId);
+
+        CartDTO cart = new CartDTO();
+        cart.setProducts(purchases.get(0).getProducts());
+        cart.setTotal(purchaseService.effectiveCustomerCart(purchases));
+        return cart;
     }
 
 }
