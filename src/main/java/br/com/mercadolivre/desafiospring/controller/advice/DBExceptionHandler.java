21760b85f10bb3a88fb9c8da320a1a3025eb4d1a
Icaro Gabriel Pereira Salgado <icaro.salgado@mercadolivre.com>
2022-03-25 15:37:37 -0300

    Update src/main/java/br/com/mercadolivre/desafiospring/controller/advice/DBExceptionHandler.java

--- src/main/java/br/com/mercadolivre/desafiospring/controller/advice/DBExceptionHandler.java
+++ src/main/java/br/com/mercadolivre/desafiospring/controller/advice/DBExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class DBExceptionHandler {
 
     @ExceptionHandler(value = {DataBaseWriteException.class})
