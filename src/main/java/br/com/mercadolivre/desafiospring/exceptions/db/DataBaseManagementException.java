b284fc1864a54a2d94511ae771fa51aa4322cd8f
Icaro Gabriel Pereira Salgado <icaro.salgado@mercadolivre.com>
2022-03-27 18:33:54 -0300

    Feat adding db auto creation (#82)
    * feat: adding a DataBaseException class
    
    * fix: fixing database read exception tratative
    
    * feat: adding connect method to FileManager class
    
    * feat: adding default databases
    
    * feat: implementing DataBase auto creation
    
    close #48
    clo #52

---
+++ src/main/java/br/com/mercadolivre/desafiospring/exceptions/db/DataBaseManagementException.java
@@ -0,0 +1,7 @@
+package br.com.mercadolivre.desafiospring.exceptions.db;
+
+public class DataBaseManagementException extends Exception{
+    public DataBaseManagementException(String msg) {
+        super(msg);
+    }
+}
